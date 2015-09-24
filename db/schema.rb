24d9d85ac020c1dffcc003bc22870b261a124ba2
Blake Farha <bafarha@gmail.com>
2015-09-24 23:08:08 +0200

    added columns to user table and ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150923194459) do
+ActiveRecord::Schema.define(version: 20150924210419) do
 
   create_table "jokes", force: :cascade do |t|
     t.string   "title"
@@ -49,5 +49,14 @@ ActiveRecord::Schema.define(version: 20150923194459) do
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
